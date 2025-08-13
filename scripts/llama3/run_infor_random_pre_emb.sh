1dd49cbf8a5ea5b8cba565e4fec9a5715fb4fd7f
tanghuaze02 <tanghuaze02@meituan.com>
2025-08-14 02:16:40 +0800

    feat(dataset): add full float precision dataset * add dataset for all * add dataset running in bash

--- scripts/llama3/run_infor_random_pre_emb.sh
+++ scripts/llama3/run_infor_random_pre_emb.sh
@@ -17,3 +17,14 @@ for repeat_time in 1 2 3 4 5; do
         echo "No s3upload.sh script found. Result will be saved locally."
     fi
 done
+
+for repeat_time in 1 2 3 4 5; do
+    experiment_name="${model}_${dataset}-float32_${method}_${repeat_time}"
+    python3 main.py evaluation=infor_random_in_context_pre_emb dataset=mmlu_pre_emb_llama3_float32 model=no_model model.config.layer_num=32 | tee "log/${experiment_name}.txt"
+
+    if [ -f "scripts/s3upload.sh" ]; then
+        bash scripts/s3upload.sh "log/${experiment_name}"
+    else
+        echo "No s3upload.sh script found. Result will be saved locally."
+    fi
+done
